ea82c3fbdddd4e9024d35816ad7b28a71fc29770
Gerardo <ggrrdosorio53@gmail.com>
2025-03-27 21:43:20 -0600

    variable de entorno

--- app/utils/apiLogin/authApi.ts
+++ app/utils/apiLogin/authApi.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://localhost:7137/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Función para iniciar sesión y almacenar el token con expiración
 export async function loginRequest(correo: string, contrasena: string) {
